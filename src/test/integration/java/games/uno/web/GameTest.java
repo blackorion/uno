98fa8b70e353122ab53658b8b30e902472ed33fb
orion <sergej.ivanov@nw.rt.ru>
2015-09-24 15:53:55 +0300

    game integration test

---
+++ src/test/integration/java/games/uno/web/GameTest.java
@@ -0,0 +1,55 @@
+package games.uno.web;
+
+import games.uno.UnoApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.lang.reflect.Type;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = UnoApplication.class)
+public class GameTest {
+    Game game = new Game();
+
+    @Test
+    public void WhenFirstPlayerEnterTheSite_JoinsNewCreatedGame() {
+        game.connect();
+    }
+
+    private class Game {
+        private static final String URL = "ws://127.0.0.1:8080/ws";
+
+        public void connect() {
+            WebSocketClient transport = new StandardWebSocketClient();
+            WebSocketStompClient stompClient = new WebSocketStompClient(transport);
+            stompClient.setMessageConverter(new StringMessageConverter());
+            stompClient.connect(URL, makeSessionHandler());
+        }
+    }
+
+    private StompSessionHandler makeSessionHandler() {
+        return new StompSessionHandlerAdapter() {
+            @Override
+            public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                session.subscribe("/game.players", new StompFrameHandler() {
+                    @Override
+                    public Type getPayloadType(StompHeaders stompHeaders) {
+                        return String.class;
+                    }
+
+                    @Override
+                    public void handleFrame(StompHeaders stompHeaders, Object o) {
+                        System.out.println(o);
+                    }
+                });
+            }
+        };
+    }
+}
